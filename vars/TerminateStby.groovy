e0a7589e2d2e206594ea91ba70bd2a4ec27b7ad8
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-21 19:04:47 +0530

    Update TerminateStby.groovy

--- vars/TerminateStby.groovy
+++ vars/TerminateStby.groovy
@@ -1,10 +1,10 @@
 def call() { 
- /* def props = readJSON file: 'resources/foo/Parameters.json'
- /* last_started = env.STAGE_NAME
+  def props = readJSON file: 'resources/foo/Parameters.json'
+  last_started = env.STAGE_NAME
                 sh """
                 . ${PYTHON_FW_ENV}
                 cd \$OOW_HOME/main/otmociwf
                 echo '${params.EnvironmentName}'
                 python invoke_workflow.py --workflow=terminate_dr_workflow --release=${params.OTM_RELEASE} --environment_name=${params.EnvironmentName}
-                """ */
+                """ 
 }
