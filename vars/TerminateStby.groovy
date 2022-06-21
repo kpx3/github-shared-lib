3638b72338362abd73f3eeaf888594016b7a79f1
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-21 23:17:22 +0530

    Update TerminateStby.groovy

--- vars/TerminateStby.groovy
+++ vars/TerminateStby.groovy
@@ -1,12 +1,12 @@
 import groovy.json.JsonSlurper
 def call() { 
   def props = libraryResource("foo/Parameters.json")
-  def jsonSlurper = new JsonSlurper()
-  cfg = jsonSlurper.parseText(props)
+  cfg = JsonSlurper.parseText(props)
+  assert cfg instanceof Map
   sh "echo ${cfg}"
   last_started = env.STAGE_NAME
                 sh """
-                . ${cfg.PYTHON_FW_ENV}
+                . ${cfg[PYTHON_FW_ENV]}
                 cd \$OOW_HOME/main/otmociwf
                 echo '${params.EnvironmentName}'
                 python invoke_workflow.py --workflow=terminate_dr_workflow --release=${params.OTM_RELEASE} --environment_name=${params.EnvironmentName}
