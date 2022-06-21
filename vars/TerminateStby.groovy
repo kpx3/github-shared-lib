55e2c4c0d1fe3b92eee2a98edd10fb991e0d4a20
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-21 18:28:48 +0530

    Create TerminateStby.groovy

---
+++ vars/TerminateStby.groovy
@@ -0,0 +1,9 @@
+def call() { 
+last_started = env.STAGE_NAME
+                sh """
+                . ${PYTHON_FW_ENV}
+                cd \$OOW_HOME/main/otmociwf
+                echo '${params.EnvironmentName}'
+                python invoke_workflow.py --workflow=terminate_dr_workflow --release=${params.OTM_RELEASE} --environment_name=${params.EnvironmentName}
+                """
+}
