42f99a9d8790551ada6a6943e34b3b78b23a34bb
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-22 11:45:09 +0530

    Create PostSwitchoverTesting.groovy

---
+++ vars/PostSwitchoverTesting.groovy
@@ -0,0 +1,13 @@
+import groovy.json.JsonSlurperClassic
+def call() { 
+  def props = libraryResource("foo/Parameters.json")
+  def data = new JsonSlurperClassic().parseText(props)
+  sh "echo ${data}"
+  last_started = env.STAGE_NAME
+                 def testGroup = [:]
+                 testGroup['sanitlyLite'] = {
+                 echo 'Build SanityLite Test'
+                 build(job: './QA/22A_DR_SanityLite')
+                 }
+                 parallel testGroup
+}
