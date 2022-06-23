3d3cc60ba9cf3d506a3dfa4ecd9016cbfdc9f2cd
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-23 21:12:35 +0530

    Update deriveJobParams.groovy

--- vars/deriveJobParams.groovy
+++ vars/deriveJobParams.groovy
@@ -11,7 +11,7 @@ def call(Map config = [:]) {
   def db_name = data.db_name
   def otmRelease = data.otmRelease
  
-  evaluate('${serviceNameSuffix}')
+  evaluate '${serviceNameSuffix}'
     properties([
         parameters([
         
