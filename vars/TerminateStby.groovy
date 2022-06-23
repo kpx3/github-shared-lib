54324046109eb87bbc89d3424945a113c06c20a2
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-23 17:59:31 +0530

    Update TerminateStby.groovy

--- vars/TerminateStby.groovy
+++ vars/TerminateStby.groovy
@@ -3,10 +3,10 @@ def call(Map config = [:]) {
   def props = libraryResource("foo/Parameters.json")
   def data = new JsonSlurperClassic().parseText(props)
   sh "echo ${data} ${config.job}"
-  def p1 = config.job+data.ServiceNameSuffix
+  def p1 = config.job+data.serviceNameSuffix
   last_started = env.STAGE_NAME
                 sh """
                 python ${data.PYTHON_FW_ENV}              
-                echo ' ${p1} ${data.EnvironmentName} ${data.OTM_RELEASE} ${data.ServiceNameSuffix}'
+                echo ' ${p1} ${data.EnvironmentName} ${data.OTM_RELEASE} ${data.serviceNameSuffix}'
                 """ 
 }
