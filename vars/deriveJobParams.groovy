1f83716039b4d061e43f70f4a8a4709aa4f82b1c
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-23 20:40:37 +0530

    Update deriveJobParams.groovy

--- vars/deriveJobParams.groovy
+++ vars/deriveJobParams.groovy
@@ -2,6 +2,10 @@ import groovy.json.JsonSlurperClassic
 def call(Map config = [:]) { 
   def props = libraryResource("foo/Parameters.json")
   def data = new JsonSlurperClassic().parseText(props)
+  def serviceNameSuffix = data.serviceNameSuffix
+  def environmentName = data.environmentName
+  def db_name = data.db_name
+  def otmRelease = data.otmRelease
   def JOB_NAME = config.job
     properties([
         parameters([
@@ -10,15 +14,15 @@ def call(Map config = [:]) {
        
         stringParam(name: 'Test', defaultValue: JOB_NAME, description: 'Give the job name'),
           
-          stringParam(name: 'SuffixName', defaultValue: '${data.serviceNameSuffix}', description: 'Give the suffix name'),
+          stringParam(name: 'SuffixName', defaultValue: serviceNameSuffix, description: 'Give the suffix name'),
         
-        stringParam(name: 'EnvironmentName', defaultValue: data.environmentName, description: 'Give the environment name'),
+        stringParam(name: 'EnvironmentName', defaultValue: environmentName, description: 'Give the environment name'),
 
         choiceParam(name: 'SkipDBReset', choices: ['False', 'True'], description: 'Skips DB reset'),
         
-        stringParam(name: 'DB_NAME', defaultValue: data.db_name, description: 'db name'),
+        stringParam(name: 'DB_NAME', defaultValue: db_name, description: 'db name'),
         
-        stringParam(name: 'OTM_RELEASE', defaultValue: data.otmRelease, description: 'new otm release'),
+        stringParam(name: 'OTM_RELEASE', defaultValue: otmRelease, description: 'new otm release'),
         
         stringParam(name: 'OTM_VERSION', defaultValue: 'latest', description: 'new latest / image name like otm20c-time-stamp'),
         
