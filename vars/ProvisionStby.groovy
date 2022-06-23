2eb1caf4108abc4f4251c8c640acbd63e9f46643
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-23 14:59:41 +0530

    Update ProvisionStby.groovy

--- vars/ProvisionStby.groovy
+++ vars/ProvisionStby.groovy
@@ -7,8 +7,8 @@ last_started = env.STAGE_NAME
                 echo 'Create DR'
                 sh """
                 . ${data.PYTHON_FW_ENV}
-                cd \$OOW_HOME/main/otmociwf
+               
                 echo '${params.EnvironmentName}'
-                python invoke_workflow.py --workflow=create_dr_workflow --release=${params.OTM_RELEASE} --environment_name=${params.EnvironmentName} --skip_stby_db_creation=False
+              
                 """
 }
