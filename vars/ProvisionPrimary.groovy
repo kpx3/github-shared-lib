8681ac7ac9c15dad752a08deb5a78b4f4e5446e2
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-23 15:01:57 +0530

    Update ProvisionPrimary.groovy

--- vars/ProvisionPrimary.groovy
+++ vars/ProvisionPrimary.groovy
@@ -6,8 +6,6 @@ def call() {
                 echo 'Provisioning '
                 sh """
                 . ${data.PYTHON_FW_ENV}
-                cd \$OOW_HOME/main/otmociwf
-                python -u invoke_workflow.py --workflow=provisioning_workflow --release=${params.data.OTM_RELEASE} --patch_version=${params.data.OTM_VERSION} --db_name=${params.data.DB_NAME} --service_name_suffix=${params.SuffixName} --pod_size=${params.POD_SIZE} --db_node=otm-dev-phx-ad1-mvqnq1.exaclientsub1.zone2phx1exa.oraclevcn.com --skip_db_reset=${params.SkipDBReset}
                 """
                 echo "Provisioning done"
                 echo "Sleep for sometime before QA testing"
