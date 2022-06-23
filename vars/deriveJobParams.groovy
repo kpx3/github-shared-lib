6e8ab932470b6fb94b92f6c439d31976549fe887
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-23 19:04:08 +0530

    Update deriveJobParams.groovy

--- vars/deriveJobParams.groovy
+++ vars/deriveJobParams.groovy
@@ -1,15 +1,7 @@
 def call() {
     properties([
         parameters([
-            booleanParam(defaultValue: false, name: 'BuildAll', description: ''),
-            [$class: 'ChoiceParameter', choiceType: 'PT_CHECKBOX',
-                description: 'Choose environment category.',
-                name: 'ENVIRONMENT',
-                script: [
-                    $class: 'GroovyScript',
-                    script: [sandbox: true, script: 'return ["QA", "DEV", "PROD"]']
-                ]
-            ]
+            booleanParam(defaultValue: false, name: 'BuildAll', description: '')
         ])
     ])
 }
