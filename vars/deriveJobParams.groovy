c6c03edc828cef7ec8cc79798dcb1190789d49e7
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-23 19:15:15 +0530

    Update deriveJobParams.groovy

--- vars/deriveJobParams.groovy
+++ vars/deriveJobParams.groovy
@@ -1,7 +1,27 @@
-def call() {
+import groovy.json.JsonSlurperClassic
+def call(Map config = [:]) { 
+  def props = libraryResource("foo/Parameters.json")
+  def data = new JsonSlurperClassic().parseText(props)
+  def JOB_NAME = config.job
     properties([
         parameters([
-            booleanParam(defaultValue: false, name: 'BuildAll', description: '')
+            booleanParam(defaultValue: false, name: 'BuildAll', description: ''),
+            stringParam(name: 'SuffixName', defaultValue: serviceNameSuffix, description: 'Give the suffix name'),
+        
+        stringParam(name: 'EnvironmentName', defaultValue: environmentName, description: 'Give the environment name'),
+
+        choiceParam(name: 'SkipDBReset', choices: ['False', 'True'], description: 'Skips DB reset'),
+        
+        stringParam(name: 'DB_NAME', defaultValue: db_name, description: 'db name'),
+        
+        stringParam(name: 'OTM_RELEASE', defaultValue: otmRelease, description: 'new otm release'),
+        
+        stringParam(name: 'OTM_VERSION', defaultValue: 'latest', description: 'new latest / image name like otm20c-time-stamp'),
+        
+        choiceParam(name: 'OTM_IMAGE_BUILD_TYPE', choices: ['stable', 'nightly', 'weekly'], description: 'select stable / nighty / weekly'),
+        
+        choiceParam(name: 'POD_SIZE', choices: ['SMALL', 'MEDIUM', 'LARGE'], description: 'VM SIZE')
+        
         ])
     ])
 }
