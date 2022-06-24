4ab17cf3cf34e0d9fa4abe5a7028a5d3f714d5cc
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-24 09:36:36 +0530

    Update ProvisionStby.groovy

--- vars/ProvisionStby.groovy
+++ vars/ProvisionStby.groovy
@@ -3,10 +3,11 @@ def call(Map config =[:]) {
   def props = libraryResource("foo/Parameters.json")
   def data = new JsonSlurperClassic().parseText(props)
   def JOB_NAME = config.job
-  sh "echo ${data}"
+  sh " JOB_NAME=${config.job} echo ${data}"
 last_started = env.STAGE_NAME
                 echo 'Create DR'
                 sh """
+                 JOB_NAME=${config.job}
                 python ${data.PYTHON_FW_ENV}
                
                 echo ${data.EnvironmentName}
