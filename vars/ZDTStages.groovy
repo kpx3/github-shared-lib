ef9cc5ebc973696ba4ed188339440171807dad16
Kautilya pandey <58580448+kpx3@users.noreply.github.com>
2022-06-26 20:02:01 +0530

    Update ZDTStages.groovy

--- vars/ZDTStages.groovy
+++ vars/ZDTStages.groovy
@@ -1,16 +1,16 @@
-def call (Map config=[:]){
+def call (){
  
  pipeline {
     agent any
         stages {
             stage('ProvisionPrimary'){
                 steps {
-                     ProvisionPrimary(job:"${JOB_NAME.minus("/" + JOB_BASE_NAME)}")
+                 ProvisionPrimary(job:"${JOB_BASE_NAME}")
                 }
             }
             stage('TerminateStby'){
                 steps {
-                     TerminateStby(job:"${JOB_NAME.minus("/" + JOB_BASE_NAME)}")
+                 TerminateStby(job:"${JOB_BASE_NAME}")
                 }
             }
         }
